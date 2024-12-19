c5d26647e7a7adf4850c8be6a4f49b8669196f4e
Caleb Warren <slickwarren21@gmail.com>
2024-12-19 13:49:47 -0800

    adding eval statement for ssh

--- rancher-local/k3s-ha/e2e_create_rancher.sh
+++ rancher-local/k3s-ha/e2e_create_rancher.sh
@@ -2,16 +2,17 @@
 
 export REPO_PATH=$(realpath $(dirname ${BASH_SOURCE[0]})) 
 
-. e2e.env
-. 
+. $REPO_PATH/e2e.env
+
 cd $REPO_PATH/../../terraform/$PROVIDER
 terraform apply --auto-approve
 
 . $REPO_PATH/ips_from_terraform.sh
 cd $REPO_PATH
 
+eval `ssh-agent -s`
 
-ssh-add -k ~/.ssh/jenkins-rke-validation.pem
+ssh-add -k $SSH_PATH
 
 wait-for-ssh()
 {
