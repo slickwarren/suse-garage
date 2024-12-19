79da28f1dd06fb2c12b072ae82c3a0d7bee1ea83
Caleb Warren <slickwarren21@gmail.com>
2024-12-19 13:06:35 -0800

    moving waitForSSH here

--- rancher-local/k3s-ha/e2e_create_rancher.sh
+++ rancher-local/k3s-ha/e2e_create_rancher.sh
@@ -3,6 +3,7 @@
 export REPO_PATH=$(realpath $(dirname ${BASH_SOURCE[0]})) 
 
 . e2e.env
+. 
 cd $REPO_PATH/../../terraform/$PROVIDER
 terraform apply --auto-approve
 
@@ -11,6 +12,43 @@ cd $REPO_PATH
 
 
 ssh-add -k ~/.ssh/jenkins-rke-validation.pem
+
+wait-for-ssh()
+{
+  export server0Access=1
+  export server1Access=1
+  export server2Access=1
+  export connectionRefused="Connection refused"
+  while (( $server0Access != 0 && $server1Access != 0 && $server2Access != 0 ));
+  do
+    if [[ $server0Access != 0 ]]; then
+      export sshOutput=$(ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=accept-new $SSH_USER@$SERVER_0 "ls -la" 2>&1)
+      if [[ $sshOutput != *"$connectionRefused"* ]]; then
+        export server0Access=0
+        continue;
+      fi
+    fi
+
+    if [[ $server1Access != 0 ]]; then
+      export sshOutput=$(ssh -i -o StrictHostKeyChecking=accept-new $SSH_KEY_PATH $SSH_USER@$SERVER_1 "ls -la" 2>&1)
+      if [[ $sshOutput != *"$connectionRefused"* ]]; then
+        export server1Access=0
+        continue;
+      fi
+    fi
+
+
+    if [[ $server2Access != 0 ]]; then
+      export sshOutput=$(ssh -i -o StrictHostKeyChecking=accept-new $SSH_KEY_PATH $SSH_USER@$SERVER_2 "ls -la" 2>&1)
+      if [[ $sshOutput != *"$connectionRefused"* ]]; then
+        export server2Access=0
+        continue;
+      fi
+    fi
+  done
+  sleep 20
+  echo "K3s Server Nodes ready for ssh"
+}
 
 wait-for-ssh
 
